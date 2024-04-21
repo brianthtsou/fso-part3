ed450b9d362689301f34d7a750c21111631c074c
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-04-20 19:14:57 -0700

    completed exercises 3.15-3.16

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const cors = require("cors");
 const app = express();
 const PhoneNumber = require("./models/phonenumber");
 
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
-app.use(express.static("dist"));
 
 morgan.token("data", (req, res) => {
   return JSON.stringify(req.body);
@@ -34,6 +34,21 @@ app.use(morganOptions);
 // };
 // app.use(requestLogger);
 
+// error handler
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
+// handler of requests with unknown endpoint
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
 app.get("/api/persons", (request, response) => {
   PhoneNumber.find({}).then((phonenumbers) => {
     response.json(phonenumbers);
@@ -49,7 +64,7 @@ app.get("/api/persons/info", async (request, response) => {
   );
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   PhoneNumber.findById(request.params.id)
     .then((phonenumber) => {
       if (!phonenumber) {
@@ -59,10 +74,7 @@ app.get("/api/persons/:id", (request, response) => {
       }
       response.json(phonenumber);
     })
-    .catch((error) => {
-      console.error("Error retrieving the phone number", error);
-      response.status(500).send({ message: "Error retrieving phone number" });
-    });
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (req, res, next) => {
@@ -108,10 +120,13 @@ app.post("/api/persons", (req, res) => {
       }
     })
     .catch((error) => {
-      console.error("Issue with adding new phone number", error);
-      res.status(500).json({ error: "Internal server error" });
+      next(error);
     });
 });
+
+app.use(unknownEndpoint);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
