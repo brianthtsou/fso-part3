a505cfbf61a1322746ee40b62776eb1cd1c4a88e
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2024-04-22 17:19:11 -0700

    completed exercises 3.20-3.21

--- models/phonenumber.js
+++ models/phonenumber.js
@@ -17,7 +17,14 @@ mongoose
 
 const phoneNumberSchema = new mongoose.Schema({
   name: { type: String, minLength: 3, required: true },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: (v) => /^\d{2,3}-\d+/.test(v),
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
 });
 
 phoneNumberSchema.set("toJSON", {
